02c946cd6e045d1abfbff69c508a9d4c358eeccf
Chinemerempatricia <chinemerempatricia02@gmail.com>
2025-03-11 20:28:52 +0600

    Added 002_string.py for string manipulation assignment

---
+++ 002_string.py
@@ -0,0 +1,17 @@
+my_sentence = "Python is fun and powerful!"
+print(len(my_sentence))
+print(my_sentence[0])
+print(my_sentence[10:30])
+print(my_sentence.lower())
+print(my_sentence.count('o'))
+print(my_sentence.index('fun'))
+print(my_sentence.replace('powerful', 'amazing'))
+new_message = 'Learning Python is exciting!'
+print(my_sentence + " " + new_message)
+print(" {} {} ".format(my_sentence, new_message))
+print(f"{my_sentence} {new_message}")
+print(dir(my_sentence))
+print(help(str.replace))
+
+
+                  
