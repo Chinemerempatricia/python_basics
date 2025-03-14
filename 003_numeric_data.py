c8015e3018a7607f56e39db974206f25f52c3949
Chinemerempatricia <chinemerempatricia02@gmail.com>
2025-03-14 19:47:25 +0600

    Add 003_numeric_data.py

---
+++ 003_numeric_data.py
@@ -0,0 +1,28 @@
+num1 = 16
+num2 = 24.32
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Exponentention:", num1 ** 4)
+print("Modules:", num1 % num2)
+
+result = (20 + 8) * 4 ** 4 / 2 - 1 
+print("Result of (20 + 8) * 4 ** 4 / 2 - 1:", result)
+print("Absolute value of -17:", abs(-17))
+
+print("Rounded to 1 decimal place:", round(num1, 1))
+print("Rounded to 2 decimal places:", round(num2, 2))
+
+print("Is num1 == num2?", num1 == num2)
+print("Is num1 != num2?", num1 != num2)
+print("Is num1 > num2?", num1 > num2)
+print("Is num1 < num2?", num1 < num2)
+print("Is num1 >= num2?", num1 >= num2)
+print("Is num1 <= num2?", num1 <= num2)
+
+print("Float to Integer:", int(num2))
+print("Integer to Float:", float(num1))
+print("String '20' to Integer:", int("20"))
+
