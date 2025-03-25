8ca518f7868db9de87cf851e60ecf78c375f178a
Chinemerempatricia <chinemerempatricia02@gmail.com>
2025-03-25 10:37:07 +0600

    Add 006_input_arithmetic.py

---
+++ 006_input_arithmetic.py
@@ -0,0 +1,22 @@
+num1 = 15
+num2 = 4.5
+addiction = num1 + num2
+subtraction = num1 -num2
+multiplication = num1 * num2
+division = num1 / num2 
+
+print("\nresults:")
+print(f"Addition: {num1} + {num2} = {num1 + num2}")
+print(f"Subtraction: {num1} - {num2} = {num1 - num2}")
+print(f"Multiplication: {num1} * {num2} = {num1 * num2}")
+print(f"Division: {num1} / {num2} = {num1 / num2}")
+
+
+
+
+
+
+
+
+
+
