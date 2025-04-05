49d0ceb4fe51a29bbbd138116716c178ed4f5d1a
Chinemerempatricia <chinemerempatricia02@gmail.com>
2025-04-06 00:26:40 +0600

    Add 026_function.py

---
+++ 026_function.py
@@ -0,0 +1,14 @@
+def greet_user():
+    print("Hello, welcome to python programming!")
+greet_user()
+
+def add_numbers(a, b):
+    print("The sum is :", a + b)
+add_numbers(7, 3)
+
+def multiply_numbers(a, b):
+    return a * b
+
+result = multiply_numbers(4, 5)
+print("The product is:", result)
+
