6350d3c60855148a0729651c880f56bc52b09bba
Chinemerempatricia <chinemerempatricia02@gmail.com>
2025-04-06 01:05:46 +0600

    Add 027_math_operations.py

---
+++ 027_math_operations.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    c = a + b
+    return c
+
+def subtract(a, b):
+    c = a - b
+    return c
+
+def multiply(a, b):
+    c = a * b
+    return c
+
+def divide(a, b):
+    c = a / b
+    return c
+
+
+
+
+    
