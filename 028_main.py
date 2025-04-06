0924a4b38593b7d29b940e75cc74c075d5e23f07
Chinemerempatricia <chinemerempatricia02@gmail.com>
2025-04-06 16:19:10 +0600

    Add math_027_operations.py

---
+++ 028_main.py
@@ -0,0 +1,15 @@
+from 027_math_operations import add, subtract, muultiply, divide
+
+print(f"Addition of 10 and  5:
+{add(10, 5)}")
+print(f"Subtraction of 10 and 5:
+{subtract(10, 5)}")
+
+print(f"Multiplication of 10 and 5:
+{multiply(10, 5)}")
+print(f"Division of 10 and 5:
+{divide(10, 5)}")
+print(f"Division of 10 by 0:
+{divide(10, 0)}")
+
+
